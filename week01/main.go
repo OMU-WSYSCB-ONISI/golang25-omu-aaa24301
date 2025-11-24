b573b926bc48ccc6f5f0d86990a1aa0ea0579b07
omu-aaa24301 <sg24507s@st.omu.ac.jp>
2025-11-24 08:43:07 +0000

    Pending changes exported from your codespace

--- week01/main.go
+++ week01/main.go
@@ -3,8 +3,27 @@ import (
 	"fmt"
 	"net/http"
 	"runtime"
+	 "net/http/httputil"
 )
 func main() {
+	 const url = "https://omu-aaa24301.github.io/"
+
+  req, _ := http.NewRequest("GET", url, nil)
+
+  dump, _ := httputil.DumpRequestOut(req, true)
+  fmt.Printf("%s", dump)
+
+  client := new(http.Client)
+  resp, err := client.Do(req)
+    if err != nil {
+		fmt.Println(err)
+		return
+	}
+  defer resp.Body.Close()
+
+
+  dumpResp, _ := httputil.DumpResponse(resp, true)
+  fmt.Printf("%s", dumpResp)
 	fmt.Printf("Go version: %s\n", runtime.Version())
 
 	http.Handle("/", http.FileServer(http.Dir("public/")))
@@ -13,4 +32,4 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Printf("Failed to launch server: %v", err)
 	}
-}   
+}
