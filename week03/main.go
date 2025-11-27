6567de49e371dde084af7069964eea5de3149528
omu-aaa24301 <sg24507s@st.omu.ac.jp>
2025-11-27 10:57:36 +0900

    add form.html and implement HTTP request handling in main.go for weeks 01 to 04

--- week03/main.go
+++ week03/main.go
@@ -1,13 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
 
 func main() {
 	// Week 03: ここに課題のコードを記述してください
 	// 詳細な課題内容はLMSで確認してください
-	
+
 	fmt.Println("Week 03 課題")
-	
+
 	// 以下に実装してください
-	
+	http.HandleFunc("/webfortune", omikujihandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("サーバーの起動に失敗")
+	}
+}
+
+func omikujihandler(w http.ResponseWriter, r *http.Request) {
+	seed := time.Now().UnixNano()
+	s := rand.New(rand.NewSource(seed))
+
+	var fortune [4]string = [4]string{"大吉", "中吉", "吉", "凶"}
+	result := fortune[s.Int31n(4)]
+
+	fmt.Fprintf(w, "今の運勢は%sです", result)
 }
--- week04/main.go
+++ week04/main.go
@@ -1,13 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
 	// Week 04: ここに課題のコードを記述してください
 	// 詳細な課題内容はLMSで確認してください
-	
+
 	fmt.Println("Week 04 課題")
-	
+
 	// 以下に実装してください
-	
+	http.HandleFunc("/info", infohandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("サーバーの起動に失敗")
+	}
+}
+
+func infohandler(w http.ResponseWriter, r *http.Request) {
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+	var time_result = time.Now().In(jst).Format("2006年01月02日 15:04:05")
+	var h map[string][]string
+	h = r.Header
+	fmt.Fprintln(w, "現在の時刻は", time_result)
+	fmt.Fprintln(w, "利用ブラウザは", h["User-Agent"])
 }
